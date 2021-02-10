439188c4fc19dd41decf2b511f73d933b7f74f29
Bruno Paz <brunopaz@gmail.com>
2021-02-09 23:06:44 -0300

    Split

--- src/Split.php
+++ src/Split.php
@@ -20,6 +20,8 @@ class Split implements \JsonSerializable
     protected $recipient;
     protected $liable;
     protected $charge_processing_fee;
+    protected $charge_recipient_processing_fee;
+    protected $is_gross_amount;
     protected $percentage;
     protected $amount;
 
@@ -58,6 +60,46 @@ class Split implements \JsonSerializable
     public function setRecipient($recipient)
     {
         $this->recipient = $recipient;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getChargeRecipientProcessingFee()
+    {
+        return $this->charge_recipient_processing_fee;
+    }
+
+    /**
+     * @param mixed $charge_recipient_processing_fee
+     *
+     * @return Split
+     */
+    public function setChargeRecipientProcessingFee($charge_recipient_processing_fee)
+    {
+        $this->charge_recipient_processing_fee = $charge_recipient_processing_fee;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getisGrossAmount()
+    {
+        return $this->is_gross_amount;
+    }
+
+    /**
+     * @param mixed $is_gross_amount
+     *
+     * @return Split
+     */
+    public function setIsGrossAmount($is_gross_amount)
+    {
+        $this->is_gross_amount = $is_gross_amount;
 
         return $this;
     }
